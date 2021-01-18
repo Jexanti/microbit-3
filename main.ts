ea11c389d9ab64d7f94b80e3d9cdd6d067d30495
Jexanti <edwardmwild@gmail.com>
2021-01-18 09:38:34 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,70 +4,136 @@ input.onButtonPressed(Button.A, function () {
     }
     basic.showNumber(numbers)
 })
+function Roll () {
+    if (die_number == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (die_number == 1) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (die_number == 2) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (die_number == 3) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (die_number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (die_number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (die_number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (die_number == 7) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . # . #
+            `)
+    }
+    if (die_number == 8) {
+        basic.showLeds(`
+            # . # . #
+            . . . . .
+            # . # . #
+            . . . . .
+            # . # . #
+            `)
+    }
+    if (die_number == 9) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . . . #
+            `)
+    }
+    if (die_number == 10) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . . . #
+            `)
+    }
+    if (die_number == 11) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . . . #
+            . # . # .
+            # . # . #
+            `)
+    }
+    if (die_number == 12) {
+        basic.showLeds(`
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            `)
+    }
+}
 input.onButtonPressed(Button.B, function () {
-    if (!(numbers == 12)) {
+    if (!(numbers == 13)) {
         numbers += 1
     }
     basic.showNumber(numbers)
 })
 input.onGesture(Gesture.Shake, function () {
-    for (let index = 0; index < 11; index++) {
+    for (let index = 0; index < 6; index++) {
         die_number = randint(1, numbers)
         die_number += -1
-        if (die_number == 0) {
-            basic.showLeds(`
-                . . . . .
-                . . . . .
-                . . # . .
-                . . . . .
-                . . . . .
-                `)
-        }
-        if (die_number == 1) {
-            basic.showLeds(`
-                . . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . .
-                `)
-        }
-        if (die_number == 2) {
-            basic.showLeds(`
-                . . . . #
-                . . . . .
-                . . # . .
-                . . . . .
-                # . . . .
-                `)
-        }
-        if (die_number == 3) {
-            basic.showLeds(`
-                # . . . #
-                . . . . .
-                . . . . .
-                . . . . .
-                # . . . #
-                `)
-        }
-        if (die_number == 4) {
-            basic.showLeds(`
-                # . . . #
-                . . . . .
-                . . # . .
-                . . . . .
-                # . . . #
-                `)
-        }
-        if (die_number == 5) {
-            basic.showLeds(`
-                . # . # .
-                . . . . .
-                . # . # .
-                . . . . .
-                . # . # .
-                `)
-        }
+        Roll()
     }
 })
 let die_number = 0

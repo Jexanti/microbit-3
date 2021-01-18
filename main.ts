805db8f004aa473c923683b5246200df721aff00
Jexanti <edwardmwild@gmail.com>
2021-01-18 09:26:39 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,58 +1,60 @@
 let die_number = 0
 input.onGesture(Gesture.Shake, function () {
-    die_number = randint(0, 5)
-    if (die_number == 0) {
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . # . .
-            . . . . .
-            . . . . .
-            `)
-    }
-    if (die_number == 1) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . .
-            `)
-    }
-    if (die_number == 2) {
-        basic.showLeds(`
-            . . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . .
-            `)
-    }
-    if (die_number == 3) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . . . .
-            . . . . .
-            # . . . #
-            `)
-    }
-    if (die_number == 4) {
-        basic.showLeds(`
-            # . . . #
-            . . . . .
-            . . # . .
-            . . . . .
-            # . . . #
-            `)
-    }
-    if (die_number == 5) {
-        basic.showLeds(`
-            . # . # .
-            . . . . .
-            . # . # .
-            . . . . .
-            . # . # .
-            `)
+    for (let index = 0; index < 11; index++) {
+        die_number = randint(0, 5)
+        if (die_number == 0) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . # . .
+                . . . . .
+                . . . . .
+                `)
+        }
+        if (die_number == 1) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                `)
+        }
+        if (die_number == 2) {
+            basic.showLeds(`
+                . . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . .
+                `)
+        }
+        if (die_number == 3) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . #
+                `)
+        }
+        if (die_number == 4) {
+            basic.showLeds(`
+                # . . . #
+                . . . . .
+                . . # . .
+                . . . . .
+                # . . . #
+                `)
+        }
+        if (die_number == 5) {
+            basic.showLeds(`
+                . # . # .
+                . . . . .
+                . # . # .
+                . . . . .
+                . # . # .
+                `)
+        }
     }
 })
